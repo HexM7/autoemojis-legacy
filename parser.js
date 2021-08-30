3ee126fa45287591837d5e33bb77b7f59eaacc10
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-08-30 14:55:55 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,14 +1,10 @@
-const Discord=require('discord.js');
-module.exports=async function(client, message){
+module.exports.parseMessage=async function(client, message){
+if(!message.content) return;
 let substringArray = get_substrings_between(message.content, ":", ":");
     let msg = message.content;
     if(!substringArray.length) return;
     substringArray.forEach(m => {
         let emoji = client.emojis.cache.find(x => x.name === m);
-        let emojiCheck = message.guild.emojis.cache.find(x => x.name === m);
-        if (emoji && !emojiCheck) {
-          checkPerm(client, message.channel, message.guild.id);
-        };
         var replace = `:${m}:`;
         var rexreplace = new RegExp(replace, 'g');
         const replaceInteger = msg.replace(/[0-9]/g, '');
@@ -30,19 +26,19 @@ let substringArray = get_substrings_between(message.content, ":", ":");
         name: message.member.nickname ? message.member.nickname : message.author.username,
         avatar: message.author.displayAvatarURL({dynamic: true})
     })
-    message.delete().catch(m => {})
-    webhook.send(msg).catch( m => {});
+    message.delete().catch(console.error)
+    webhook.send({content:msg}).catch(console.error);
     await webhook.edit({
         name: `Webhook Emojis`,
         avatar: client.user.displayAvatarURL({dynamic:true})
     })
-}
+};
+
 function get_substrings_between(str, startDelimiter, endDelimiter) {
     var contents = [];
     var startDelimiterLength = startDelimiter.length;
     var endDelimiterLength = endDelimiter.length;
     var startFrom = contentStart = contentEnd = 0;
-  
     while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
       contentStart += startDelimiterLength;
       contentEnd = strpos(str, endDelimiter, contentStart);
@@ -54,12 +50,8 @@ function get_substrings_between(str, startDelimiter, endDelimiter) {
     }
     return contents;
   }
+
   function strpos(haystack, needle, offset) {
     var i = (haystack + '').indexOf(needle, (offset || 0));
     return i === -1 ? false : i;
-  }
-  
-async function checkPerm(client, channel, role) {
-return;
-  }
-  
+  };
